d9cc517d4363517eb5dbd4e067f3d62f60623f59
joshreisner <josh@joshreisner.com>
2009-06-19 06:08:31 +0000

    eb_organizations is my test case

--- directory/index.php
+++ directory/index.php
@@ -1,27 +1,18 @@
-<? include("../include.php");
+<?php
+include("../include.php");
 drawTop();
 
-$t = new table("Organizations");
-$t->col("organization");
-$t->col("location");
-$t->col("updated", "r");
-$t->title(drawHeader());
+$t = new table();
+$t->set_column("organization");
+$t->set_column("location");
+$t->set_column("updated", "r");
+$t->set_title(drawHeader());
 
-$result = db_table("SELECT 
-						o.id,
-						o.name organization, 
-						o.phone,
-						z.city, 
-						z.state, 
-						o.zip,
-						o.lastUpdatedOn updated
-					FROM web_organizations o
-					JOIN zip_codes z ON o.zip = z.zip
-					ORDER BY o.name");
+$result = db_table("SELECT o.id, o.name organization, z.city, z.state, o.updated_date updated FROM web_organizations o JOIN zip_codes z ON o.zip = z.zip ORDER BY o.name");
 foreach ($result as &$r) {
-	$r["organization"] = draw_link("organization_view.php?id=" . $r["id"], format_string($r["organization"], 50));
-	$r["location"] = $r["city"] . ", " . $r["state"];
-	$r["updated"] = "<nobr>" . format_date($r["updated"]) . "</nobr>";
+	$r["organization"]	= draw_link("organization_view.php?id=" . $r["id"], format_string($r["organization"], 50));
+	$r["location"]		= $r["city"] . ", " . $r["state"];
+	$r["updated"]		= "<nobr>" . format_date($r["updated"]) . "</nobr>";
 }
 echo $t->draw($result);
 
