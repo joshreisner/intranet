3cf0662c334cc36f5f7c6f59b68c2b772fb581ab
joshreisner <josh@joshreisner.com>
2009-11-20 04:17:44 +0000

    fixing error on external organizations

--- external-orgs/edit.php
+++ external-orgs/edit.php
@@ -1,5 +1,7 @@
-<?
-$included = !@include('../include.php');
+<?php
+$included = isset($_josh);
+
+if (!$included) include('../include.php');
 
 if ($posting) {
 	$id = db_save('external_orgs');
@@ -7,18 +9,20 @@ if ($posting) {
 	if (getOption('channels')) db_checkboxes('channels', 'external_orgs_to_channels', 'org_id', 'channel_id', $id);
 	url_change_post('./type.php?id=' . db_grab('SELECT type_id FROM external_orgs_to_types WHERE org_id = ' . $id)); //pure hackery
 } elseif ($included) {
+	$title = getString('add_new');
 	$_josh['referrer'] = false;
 	$_josh['request']['path_query'] = 'edit.php'; //shoddy way of setting the form target
 	$r['url'] = 'http://';
 } else {
 	url_query_require();
 	echo drawTop();
+	$title = $page['title'];
 	$r = db_grab('SELECT id, title, url, description from external_orgs WHERE id = ' . $_GET['id']);
 }
 
 if ($included) $_GET['id'] = false; //type id was auto-setting checkbox
 
-$f = new form('external_orgs', @$_GET['id'], $page['name']);
+$f = new form('external_orgs', @$_GET['id'], $title);
 if (!$included) $f->set_title_prefix(drawHeader(false, ' '));
 $f->set_field(array('name'=>'types', 'type'=>'checkboxes', 'options_table'=>'external_orgs_types', 'linking_table'=>'external_orgs_to_types', 'object_id'=>'org_id', 'option_id'=>'type_id'));
 if (getOption('channels')) $f->set_field(array('name'=>'channels', 'type'=>'checkboxes', 'label'=>'Networks', 'options_table'=>'channels', 'linking_table'=>'external_orgs_to_channels', 'object_id'=>'org_id', 'option_id'=>'channel_id'));
