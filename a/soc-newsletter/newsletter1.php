b0a43de0cc2f22a96d3978300ac8580ac4e42ded
joshreisner <josh@joshreisner.com>
2009-12-11 04:14:54 +0000

    adding newsletter for eva

---
+++ a/soc-newsletter/newsletter1.php
@@ -0,0 +1,73 @@
+<?php
+include('../../include.php');
+
+echo draw_doctype();
+
+echo draw_container('head',
+	draw_meta_utf8() .
+	draw_container('title', 'Newsletter')
+);
+?>
+<body style="background-color:#fff;">
+<table width="600" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px; line-height:16px;">
+	<tr>
+		<td width="85" height="117"><img src="header1.jpg" alt="header1" width="85" height="117" border="0"/></td>
+		<td width="5" bgcolor="#8dc13f"><img src="spacer.png" width="5" height="5" border="0"/></td>
+		<td width="85" height="117"><img src="header2.jpg" alt="header1" width="85" height="117" border="0"/></td>
+		<td width="5" bgcolor="#8dc13f"><img src="spacer.png" width="5" height="5" border="0"/></td>
+		<td width="85" height="117"><img src="header3.jpg" alt="header1" width="85" height="117" border="0"/></td>
+		<td width="5" bgcolor="#8dc13f"><img src="spacer.png" width="5" height="5" border="0"/></td>
+		<td width="330" bgcolor="#3397ba" align="center"><img src="logo.png" alt="logo" width="330" height="117"/></td>
+	</tr>
+	<tr><td colspan="7" bgcolor="#8dc13f" height="5"><img src="spacer.png" width="5" height="5" border="0"/></td></tr>
+	<tr>
+		<td colspan="3" valign="top">
+			<div style="margin:16px 8px 16px 8px;">
+			
+			<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Memory Issues in the News</div>
+			<ul style="list-style-type:none; margin:10px 0px 10px 0px; padding:0px;">
+			<li style="margin-bottom:5px;padding-left:8px;"><a href="http://www.nytimes.com/2009/08/28/world/europe/28iht-moscow.html" style="color:#3397ba;">Re-Stalinization of a Moscow Subway Station</a></li>
+			<li style="margin-bottom:5px;padding-left:8px;"><a href="http://www.google.com/hostednews/afp/article/ALeqM5jfpRI3pTnVJalXXbhQcyQmmOx5Kg" style="color:#3397ba;">Russian historians fear crackdown on sensitive research</a></li>
+			<li style="margin-bottom:5px;padding-left:8px;"><a href="http://www.nytimes.com/2009/10/31/world/europe/31russia.html?_r=2" style="color:#3397ba;">Don't Gloss Over Stalin's Crimes, Medvedev Says</a></li>
+			<li style="margin-bottom:5px;padding-left:8px;"><a href="http://www.latimes.com/news/nationworld/world/la-fg-russia-stalin2-2009nov02,0,2551543.story" style="color:#3397ba;">Russia reconsiders: Was Stalin really so bad?</a></li>
+			<li style="margin-bottom:5px;padding-left:8px;"><a href="http://women.timesonline.co.uk/tol/life_and_style/women/the_way_we_live/article6937923.ece" style="color:#3397ba;">Russian textbooks attempt to rewrite history</a></li>
+			</ul>
+			
+			<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">New Institutional Members</div>
+			<p style="padding-left:8px;">The International Coalition welcomes <a href="http://www.sitesofconscience.org/about-us/institutional-members/en/">new members</a> from four new countries; Brazil, El Salvador, Poland and Uruguay.</p>
+			
+			<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">New Resource</div>
+			<p style="padding-left:8px;">Memoria Abierta’s new book <a href="http://www.memoriaabierta.org.ar/eng/principal.php">“Memoria en la Ciudad. Señales del Terrorismo de Estado en Buenos Aires”</a> (“Memories in the City: Signs of State Terrorism in Buenos Aires”), examines remnants of the suppressed history of State Terrorism 48 Buenos Aires’ neighborhoods. </p>
+			
+			<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Join Today!</div>
+			<p style="padding-left:8px;">Join the worldwide movement of organizations and individuals dedicated to helping historic sites inspire social consciousness and action. <a href="http://www.sitesofconscience.org/join-us/en/" style="color:#3397ba;">Sign up today!</a></p>
+
+			<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">About Us</div>
+			<p style="padding-left:8px;">The International Coalition of Sites of Conscience is a worldwide network of historic sites dedicated to remembering past struggles for justice and addressing their contemporary legacies. <a href="http://www.sitesofconscience.org/about-us/en/" style="color:#3397ba;">More about us</a></p>
+			
+			</div>
+		</td>
+		<td bgcolor="#8dc13f"></td>
+		<td colspan="3" valign="top">
+		<div style="margin:13px 26px 13px 26px;">
+		<h3 style="font-weight:normal;font-size:14px;color:#9a9a9a;margin:0px;">December 2009</h3>
+		<!--<h1 style="color:#3397ba;font-size:14px;">We’re pleased to be ending this year with a new beginning!</h1>
+		<img src="content.jpg" alt="content" style="float:right; margin-left:10px;" width="158" height="165"/>-->
+		
+		<p><img src="http://www.sitesofconscience.org/wp-content/uploads/untitled2.JPG" style="float:right; margin:4px 0px 0px 10px;" width="168" height="244" border="0"/>Accepting our past the way it is, is mature citizenship”, said President Medvedev in November, adding his voice to the debate that has divided Russia for more then half a century, and recently reached fever-pitch: how to remember Stalin and his regime.  Despite Medvedev’s words, many historians and human rights activists believe Russia is drifting towards “re-Stalinizing” –  presenting a single, positive image of Stalin. In the past year, the government’s decisions – <a href="http://women.timesonline.co.uk/tol/life_and_style/women/the_way_we_live/article6937923.ece">issuing a new text book</a> that glosses over Stalin’s crimes, <a href="http://www.nytimes.com/2009/08/28/world/europe/28iht-moscow.html">restoring a Soviet-era mural</a> with an inscription praising Stalin in a Moscow subway station, and <a href="http://www.google.com/hostednews/afp/article/ALeqM5jfpRI3pTnVJalXXbhQcyQmmOx5Kg">clamping down on historical research</a> – inspire fear that whitewashing the memory of Stalin’s repressive rule can set the stage for human rights abuse today.</p>
+		<p>In this context, Sites of Conscience in Russia are creating new spaces for ongoing dialogue on all aspects of the past and its contemporary implications.  This June, the Gulag Museum at Perm-36, coordinator of <a href="http://www.sitesofconscience.org/resources/networks/russia/">the Russian Sites of Conscience Network</a>, launched the <a href="http://www.sitesofconscience.org/activities/collaborations/en/#gulag">Summer School of Museology</a>, building the capacity of museums to raise public awareness of the history and consequences of totalitarianism. Similarly, with the <a href="http://www.sitesofconscience.org/activities/collaborations/en/#Krasnoyarsk">“Moments of History”</a> workshop, the Krasnoyarsk Museum Center provided training and peer exchange for museums in the Krasnoyarsk province creating exhibits on Soviet history that stimulate dialogue on issues facing Russia today.</p>
+		<p>Across the world, Sites of Conscience offer space for their communities to confront their most pressing issues:
+		<p><img src="http://www.sitesofconscience.org/wp-content/uploads/aanmjpg.jpg" style="float:left; margin:0px 10px 0px 0px;" width="200" height="141" border="0"/>As President Obama strives to craft a new vision for US immigration, public debates – rife with vitriol and misinformation – leave out lessons of how we have confronted immigration in the past. This September, Immigration Sites of Conscience from coast to coast launched <a href="http://www.sitesofconscience.org/wp-content/uploads/ISoC_Site_Programs_9.091.pdf">new programs</a> for people to come together across difference, learn how immigration has shaped their community to date, and talk <a href="http://www.sitesofconscience.org/activities/collaborations/en/#Immigration">Face to Face</a> about today’s challenges. A blitz of <a href="http://www.sitesofconscience.org/wp-content/uploads/09-30_Museums-Take-on-Immigration-Debate-with-Exhibits1.pdf">media coverage</a> helped spur this new national conversation.</p>
+		<p>In the midst of Cambodia’s landmark trial of architects of the Khmer Rouge genocide, the Tuol Sleng Genocide Museum, based in a former Khmer Rouge prison, is launching its <a href="http://www.sitesofconscience.org/activities/en/#Tuol%20Sleng">first educational program</a>. Through the Coalition’s <a href="http://www.sitesofconscience.org/resources/project-support-fund/en/">Project Support Fund</a>, the Museum will bring Cambodian students to the site to learn from its past and discuss its legacies today.</p>
+		<p>Communities across the country identified dozens of local sites related to Liberia’s 14-year conflict as crucial spaces for healing and reconciliation.  To support them, Coalition members Civic Initiative and Liberia Media Center worked with the Secretariat to make memorialization the <a href="http://www.sitesofconscience.org/activities/en/#TRC">first among 38 recommendations</a> of the Truth and Reconciliation Commission at the Liberian National Reconciliation Conference this June.</p>
+
+
+		<img src="signature.png" alt="signature" width="82" height="32"/>
+		<p>Liz Ševčenko<br>
+		Director, International Coalition of Sites of Conscience</p>
+		</div>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
