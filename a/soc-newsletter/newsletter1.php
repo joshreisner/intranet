2d3481b0a15f947e042c72bc32e51ee4c65dfb17
joshreisner <josh@joshreisner.com>
2009-12-16 20:30:11 +0000

    asdf

--- a/soc-newsletter/newsletter1.php
+++ a/soc-newsletter/newsletter1.php
@@ -8,7 +8,8 @@ echo draw_container('head',
 	draw_container('title', 'Newsletter')
 );
 ?>
-<body style="background-color:#fff;">
+<body style="background-color:#fff;margin:0px;">
+<div style="padding:15px;">
 <table width="650" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px; line-height:16px;">
         <tr>
                <td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header1.jpg" alt="header1" width="85" height="117" border="0"/></td>
@@ -21,16 +22,17 @@ echo draw_container('head',
         </tr>
         <tr><td colspan="7" bgcolor="#8dc13f" height="5"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td></tr>
         <tr>
-               <td colspan="3" valign="top">
-                       <div style="margin:16px 8px 16px 8px;">
+               <td colspan="3" valign="top" style="padding:8px;">
+				<!--left side bar-->
+                       <div style="margin:8px 0px 8px 0px;">
                        
                        <div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Memory Issues in the News</div>
                        <ul style="list-style-type:none; margin:10px 0px 10px 0px; padding:0px;">
-                       <li style="margin-bottom:5px;padding-left:8px;"><a style="color:#3397ba;" href="http://www.nytimes.com/2009/08/28/world/europe/28iht-moscow.html">Re-Stalinization of a Moscow Subway Station</a></li>
-                       <li style="margin-bottom:5px;padding-left:8px;"><a style="color:#3397ba;" href="http://www.google.com/hostednews/afp/article/ALeqM5jfpRI3pTnVJalXXbhQcyQmmOx5Kg">Russian Historians Fear Crackdown on Sensitive Research</a></li>
-                       <li style="margin-bottom:5px;padding-left:8px;"><a style="color:#3397ba;" href="http://www.nytimes.com/2009/10/31/world/europe/31russia.html?_r=2">Don't Gloss over Stalin's Crimes, Medvedev Says</a></li>
-                       <li style="margin-bottom:5px;padding-left:8px;"><a style="color:#3397ba;" href="http://www.latimes.com/news/nationworld/world/la-fg-russia-stalin2-2009nov02,0,2551543.story">Russia Reconsiders: Was Stalin Really so Bad?</a></li>
-                       <li style="margin-bottom:5px;padding-left:8px;"><a style="color:#3397ba;" href="http://women.timesonline.co.uk/tol/life_and_style/women/the_way_we_live/article6937923.ece">Russian Textbooks Attempt to Rewrite History</a></li>
+                       <li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.nytimes.com/2009/08/28/world/europe/28iht-moscow.html">Re-Stalinization of a Moscow Subway Station</a></li>
+                       <li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.google.com/hostednews/afp/article/ALeqM5jfpRI3pTnVJalXXbhQcyQmmOx5Kg">Russian Historians Fear Crackdown on Sensitive Research</a></li>
+                       <li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.nytimes.com/2009/10/31/world/europe/31russia.html?_r=2">Don't Gloss over Stalin's Crimes, Medvedev Says</a></li>
+                       <li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.latimes.com/news/nationworld/world/la-fg-russia-stalin2-2009nov02,0,2551543.story">Russia Reconsiders: Was Stalin Really so Bad?</a></li>
+                       <li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://women.timesonline.co.uk/tol/life_and_style/women/the_way_we_live/article6937923.ece">Russian Textbooks Attempt to Rewrite History</a></li>
                        </ul>
                        
                        <div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">New Institutional Members</div>
@@ -48,11 +50,13 @@ echo draw_container('head',
                        </div>
                </td>
                <td bgcolor="#8dc13f"></td>
-               <td colspan="3" valign="top" style="font-size:12px;line-height:18px;">
-               <div style="margin:13px 26px 13px 26px;">
+               <td colspan="3" valign="top" style="padding:13px; font-size:12px; line-height:18px;">
+               <div style="margin:0px 13px 0px 13px;">
                <h3 style="font-weight:normal;font-size:14px;color:#9a9a9a;margin:0px;">December 2009</h3>
-               <!--<h1 style="color:#3397ba;font-size:14px;">We’re pleased to be ending this year with a new beginning!</h1>
-               <img src="content.jpg" alt="content" style="float:right; margin-left:10px;" width="158" height="165"/>-->
+               				<!--right side content-->
+
+               
+               <!--<h1 style="color:#3397ba;font-size:14px;">We’re pleased to be ending this year with a new beginning!</h1>-->
                
                <p><div style="float:right;margin:4px 0px 1px 10px;width:168px;color:#999;font-size:11px;line-height:14px;"><img src="http://www.sitesofconscience.org/wp-content/uploads/untitled2.JPG" width="168" height="244" border="0" style="margin-bottom:3px;"/><br/>Krasnoyarsk Museum Center builds <a style="color:#777" href="http://www.sitesofconscience.org/activities/collaborations/en/#gulag">exhibit on Gulag history</a> around an everyday object, the electric iron.</div>“Accepting our past the way it is, is mature citizenship”, said President Medvedev in November, adding his voice to the debate that has divided Russia for more then half a century, and recently reached fever-pitch: how to remember Stalin and his regime. Despite Medvedev’s words, many historians and human rights activists believe Russia is drifting towards “re- Stalinizing – presenting a single, positive image of Stalin. Begun under Putin’s leadership, the government’s decisions over the past few years – issuing a new text book that glosses over Stalin’s crimes, restoring a Soviet-era mural with an inscription praising Stalin in a Moscow subway station, and clamping down on historical research – inspire fear that whitewashing the memory of Stalin’s repressive rule can set the stage for human rights abuse today.
                In this context, Sites of Conscience in Russia are creating new spaces for ongoing dialogue on all aspects of the past and its contemporary implications. This June, the Gulag Museum at Perm-36, coordinator of the <a style="color:#3397ba;" href="http://www.sitesofconscience.org/resources/networks/russia/">Russian Sites of Conscience Network</a>, launched the <a style="color:#3397ba;" href="http://www.sitesofconscience.org/activities/collaborations/en/#gulag">Summer School of Museology</a>, building the capacity of museums to raise public awareness of the history and consequences of totalitarianism. Similarly, with the <a style="color:#3397ba;" href="http://www.sitesofconscience.org/activities/collaborations/en/#Krasnoyarsk">“Moments of History” workshop</a>, the Krasnoyarsk Museum Center provided training and peer exchange for museums in the Krasnoyarsk province creating exhibits on Soviet history that stimulate dialogue on issues facing Russia today.</p>
@@ -66,5 +70,6 @@ echo draw_container('head',
                </td>
         </tr>
 </table>
+</div>
 </body>
 </html>
