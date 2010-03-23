7bab324d06d56ebd46a00b354afe3f8215e24d27
joshreisner <josh@joshreisner.com>
2010-03-23 03:43:00 +0000

    adding newsletter for eva

---
+++ a/soc-newsletter/newsletter2.php
@@ -0,0 +1,70 @@
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Newsletter</title>
+</head>
+<body style="background-color:#fff;margin:0px;">
+<div style="padding:15px;">
+<table width="650" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px; line-height:16px;">
+<tr>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header1.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header2.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header3.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="380" bgcolor="#3397ba" align="center"><img border=0 src="http://members.sitesofconscience.org/a/soc-newsletter/newsletter-logo.png" alt="logo" width="380" height="117"/></td>
+</tr>
+<tr><td colspan="7" bgcolor="#8dc13f" height="5"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td></tr>
+<tr>
+<td colspan="3" valign="top" style="padding:8px;">
+<div style="margin:8px 0px 8px 0px;">
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Memory Issues in the News</div>
+<ul style="list-style-type:none; margin:10px 0px 10px 0px; padding:0px;">
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://turtlebay.foreignpolicy.com/posts/2010/01/27/un_report_lumps_us_in_with_stalin_pinochet"target="new">U.N. Report on Secret Detention Lumps U.S. in with Stalin, Pinochet</a></li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://news.bbc.co.uk/2/hi/uk_news/8483524.stm"target="new">Secret Terror Detentions Role Denied by UK</a></li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.google.com/hostednews/ap/article/ALeqM5gwcj8nPbGZkLQNPcj2iTAGe4bvYwD9DUO3NO0"target="new">Russia Seeks to Suppress UN Secret Prisons Report </a></li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www1.voanews.com/english/news/usa/UN-Human-Rights-Council-Postpones-Discussion-of-Secret-Detentions-Report-87172062.html"target="new">UN Human Rights Council Postpones Discussion of 'Secret Detentions' Report</a></li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;">More News:</li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.nytimes.com/2010/02/23/world/middleeast/23mideast.html?emc=eta1"target="new">Clashes Follow Claim of 2 Israeli Heritage Sites in West Bank</a></li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.google.com/hostednews/afp/article/ALeqM5j9LRRzD7dPc0tsjSdg6GwDzW7R9g"target="new">German Row over WWII Museum Eased</a></li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.guardian.co.uk/world/2009/jan/15/sarkozy-france-history-museums"target="new">Sarkozy Plan for History Museum Fails to Stir France</a></li>
+</ul>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">New Institutional Members</div>
+<ul style="list-style-type:none; margin:10px 0px 10px 0px; padding:0px;">
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.sitesofconscience.org/about-us/institutional-members/en/">Birmingham Civil Rights Museum</a> (Birmingham, USA)</li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.sitesofconscience.org/about-us/institutional-members/en/">National Center for Civil and Human Rights</a> (Atlanta, USA)</li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.sitesofconscience.org/about-us/institutional-members/en/">Research and Documentation Center</a> (Sarajevo, Bosnia)</li>
+<li style="margin:0px 0px 5px 0px;padding:0px 0px 0px 8px;"><a style="color:#3397ba;" href="http://www.sitesofconscience.org/about-us/institutional-members/en/">Simon&rsquo;s Town Museum</a> (Simon&rsquo;s Town, South Africa)</li>
+</ul>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Join Today!</div>
+<p style="padding-left:8px; margin:10px 0px 10px 0px;">Join the worldwide movement of organizations and individuals dedicated to helping historic sites inspire social consciousness and action. <a style="color:#3397ba;" href="http://www.sitesofconscience.org/join-us/en/">Sign up today!</a></p>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">About Us</div>
+<p style="padding-left:8px; margin:10px 0px 10px 0px;">The International Coalition of Sites of Conscience is a worldwide network of historic sites dedicated to remembering past struggles for justice and addressing their contemporary legacies. <a style="color:#3397ba;" href="http://www.sitesofconscience.org/about-us/en/">More about us</a></p>
+</div>
+</td>
+<td bgcolor="#8dc13f"></td>
+<td colspan="3" valign="top" style="padding:13px; font-size:12px; line-height:18px;">
+<div style="margin:0px 13px 0px 13px;">
+<h3 style="font-weight:normal;font-size:14px;color:#9a9a9a;margin:0px;">March 2010</h3>
+<br>
+When the UN began investigating secret detention practices, it didn&rsquo;t start with Guant&aacute;namo, but with the Gulag.
+Its <a style="color:#3397ba;"href="http://www.sitesofconscience.org/wp-content/uploads/2010_01_27_PUB_UN_Report_on_Secret_Detention.pdf"target="new">222-page report</a> analyzes War on Terror practices as part of a long history of secret detention by governments around the world. Was this unfairly <a style="color:#3397ba;" href="&rdquo;" http://turtlebay.foreignpolicy.com/posts/2010/01/27/un_report_lumps_us_in_with_stalin_pinochet&rdquo;target=&rdquo;new&rdquo;> &ldquo;lumping the U.S. in with Stalin and Pinochet&rdquo;</a>, or providing important perspective on recent policies? Several countries including the US, UK, Russia and Nigeria have <a style="color:#3397ba;"href="http://news.bbc.co.uk/2/hi/uk_news/8483524.stm"target="new">opposed the report's findings and methodology</a>. Some even sought to <a style="color:#3397ba;"href="http://www.google.com/hostednews/ap/article/ALeqM5gwcj8nPbGZkLQNPcj2iTAGe4bvYwD9DUO3NO0"target="new">suppress public access</a> to it, resulting in the <a style="color:#3397ba;"href="http://www1.voanews.com/english/news/usa/UN-Human-Rights-Council-Postpones-Discussion-of-Secret-Detentions-Report-87172062.html"target="new">postponement of a scheduled presentation</a> of the report before the UN Human Rights Council this March.
+<br>But around the world societies have taken the bold step of throwing open formerly hidden cells and camps as Sites of Conscience, to build public awareness of what happened there and what lessons it holds for the future. In Argentina, <a style="color:#3397ba;" href="http://www.sitesofconscience.org/sites/memoria-abierta/en/"target="new">Memoria Abierta</a> spent years exposing detention centers tucked across Buenos Aires, and successfully advocated for the massive ESMA (Naval Academy) campus, where over 5,000 people were detained or disappeared during the dictatorships, to be opened as a &ldquo;Space for Memory and Human Rights.&rdquo; At the <a style="color:#3397ba;" href="http://www.sitesofconscience.org/sites/gulag-museum/en/"target="new">GULAG Museum at Perm-36</a> in Russia, students visit the imposing cells and work yards of the former Stalinist labor camp to debate today&rsquo;s challenges to democracy. <div style="right;margin:4px 10px 1px 0px;width:320px;color:#999;font-size:11px;line-height:14px;"><img src="http://www.sitesofconscience.org/wp-content/uploads/Con-Hill.jpg" width="320" height="187" border="0" style="margin-bottom:3px;"/><br/>
+South Africa&rsquo;s Constitutional Court stands alongside the Old Fort prison at <a style="color:#777" href="http://www.sitesofconscience.org/sites/constitution-hill/en/"target="new">Constitution Hill</a>. Photo: Angela Buckland</div> <br> In South Africa, visitors tour <a style="color:#3397ba;" href="http://www.sitesofconscience.org/sites/constitution-hill/en/"target="new">Constitution Hill</a> &ndash; the site of the nation&rsquo;s Constitutional Court built alongside the Old Fort, a former Apartheid prison &ndash; to remember the perversion of justice in the past, and debate questions of justice in South Africa&rsquo;s present.
+These Sites of Conscience, along with others across the world, recognized the need to help citizens see and understand detention practices by making prison sites visible in the landscape.</p>
+<p> In the past few months, Sites of Conscience took these new steps to open dialogue on the most pressing questions their communities face: </p>
+<ul style="list-style-type:none;margin:0px;padding:0px;">
+<li style="background-image:url(http://members.sitesofconscience.org/a/soc-newsletter/bullet.png);background-position-y:4px;background-repeat:no-repeat;padding:0px 0px 10px 30px;min-height:50px;"><div style="float:left;margin:4px 10px 1px 0px;width:200px;color:#999;font-size:11px;line-height:14px;"><img src="http://www.sitesofconscience.org/wp-content/uploads/LWM.jpg" style="margin-bottom:3px" width="200" height="177" border="0"/><br/> <a style="color:#777;" href="http://www.sitesofconscience.org/sites/liberation-war-museum/en/"> Liberation War Museum</a> unveils the architectural model for its new museum.</div> In Bangladesh, the Liberation War Museum unveiled the architectural model of the museum&rsquo;s landmark <a style="color:#3397ba;" href="http://www.sitesofconscience.org/activities/en/#Liberation War Museum"target="new">new facility</a>, to be constructed in Dhaka. And the Jamalpur Gandhi Ashram laid the first foundation stone of its <a style="color:#3397ba;" href="http://www.sitesofconscience.org/activities/en/#Jamalpur"target="new">new Freedom Struggle Museum</a> in Jamalpur.
+Both museums will be major new forums to learn from Bangladesh&rsquo;s struggle for independence in order to envision a future free of religious and ethnic violence. </li>
+<li style="background-image:url(http://members.sitesofconscience.org/a/soc-newsletter/bullet.png);background-position-y:4px;background-repeat:no-repeat;padding:0px 0px 10px 30px;min-height:50px;"> What role can historic sites play in promoting a &ldquo;green&rdquo; world? This March, a first working group of sites around the world will explore the concept of &ldquo;Environmental Sites of Conscience&rdquo; at the <a style="color:#3397ba;"href="http://www.sitesofconscience.org/activities/en/#Environmental"target="new"> National Council of Public History&rsquo;s annual conference</a>. The group will envision how sites that remember human impact on our environment over time can engage people in the environmental issues we face today. </li>
+<li style="background-image:url(http://members.sitesofconscience.org/a/soc-newsletter/bullet.png);background-position-y:4px;background-repeat:no-repeat;padding:0px 0px 10px 30px;min-height:50px;"> Terez&iacute;n Memorial is bringing the lessons of totalitarianism close to home with a <a style="color:#3397ba;"href="http://www.sitesofconscience.org/activities/en/#Terezin"target="new">new program</a> for today&rsquo;s youth by remembering a familiar environment: the school. A tour of Terez&iacute;n&rsquo;s small fortress, which functioned as a Gestapo prison during WWII, will teach young visitors about the persecution of students and the impact of the Nazi regime on the school system and will help them analyze current racist and anti-Semitic messages in the Czech Republic. </li>
+<li style="background-image:url(http://members.sitesofconscience.org/a/soc-newsletter/bullet.png);background-position-y:4px;background-repeat:no-repeat;padding:0px 0px 10px 30px;min-height:50px;"> <div style="float:right;margin:4px 0px 1px 10px;width:168px;color:#999;font-size:11px;line-height:14px;"><img src="http://www.sitesofconscience.org/wp-content/uploads/TFDP1.jpg" width="160" height="142" border="0" style="margin-bottom:3px;"/><br/>Visitors explore TFDP&rsquo;s Museum of Courage and Resistance.</div> In December 2009, martial law was declared in the Philippines for the first time since the dictatorship of Ferdinand Marcos (1972 -1981).
+In this context, the Task Force Detainees of the Philippines (TFDP) is launching a bold new initiative: to share the history of human rights violations under martial law via <a style="color:#3397ba;"href="http://www.sitesofconscience.org/activities/en/#TFDP"target="new">travelling exhibits</a> that will open new dialogue about human rights &ndash; past and present &ndash; in the Philippines.</li>
+</ul>
+</div>
+</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
