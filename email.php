abec6aa0be1baaec62828171f7ca5afdebf545f0
joshreisner <josh@joshreisner.com>
2010-05-05 01:36:29 +0000

    working more on errors thing

--- email.php
+++ email.php
@@ -1,5 +1,7 @@
 <?php
 include('include.php');
 
-error_handle('test');
+//error_handle('test');
+
+array_send(array('subject'=>'testing', 'message'=>'really testing', 'url'=>$_josh['request']['url'], 'sanswww'=>$_josh['request']['sanswww']), 'http://errors.joshreisner.com/log.php');
 ?>
