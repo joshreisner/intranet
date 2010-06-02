f704e4bf1823cc47d2963d3c323a6b9498966656
joshreisner <josh@joshreisner.com>
2010-06-02 00:48:24 +0000

    adding nsletter for eva

---
+++ a/soc-newsletter/newsletter3.php
@@ -0,0 +1,182 @@
+﻿<html><head>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Newsletter</title>
+</head>
+<body style="background-color:#fff;margin:0px;">
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<div style="padding:15px;">
+<table width="650" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px; line-height:16px;">
+<tr>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header1.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header2.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header3.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="380" bgcolor="#3397ba" align="center"><img border=0 src="http://members.sitesofconscience.org/a/soc-newsletter/newsletter-logo.png" alt="logo" width="380" height="117"/></td>
+</tr>
+<tr><td colspan="7" bgcolor="#8dc13f" height="5"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td></tr>
+<tr>
+<td colspan="3" valign="top" style="padding:8px;">
+<div style="margin:8px 0px 8px 0px;">
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Memory Issues in the News</div>
+<ul style="list-style-type: none; margin: 10px 0px; padding: 0px;">
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.google.com/hostednews/afp/article/ALeqM5gCvnr75f3U9UDZj2TcGP8FrqF_Ew"
+target="new">Cambodia Makes Khmer Rouge Bastion Tourist Site: Cabinet</a></li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.independent.co.uk/news/world/asia/cambodia-puts-the-cremation-site-of-pol-pot-on-historic-tourist-trail-1919472.html"
+target="new">
+Cambodia Puts the Cremation Site of Pol Pot on 'Historic' Tourist Trail</a></li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.elpais.com/articulo/opinion/Pasa/vacaciones/gulag/elpepiopi/20100416elpepiopi_5/Tes"
+target="new">¡Pasa tus Vacaciones en el 'Gulag'!</a></li>
+</ul>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Commentary</div>
+<ul style="list-style-type: none; margin: 10px 0px; padding: 0px;">
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.guardian.co.uk/commentisfree/libertycentral/2010/apr/29/guantanamo-bay-site-conscience"
+target="new">
+Turn Guantánamo Bay into a Site of Conscience</a></li>
+</ul>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">New Institutional Members</div>
+<ul style="list-style-type: none; margin: 10px 0px; padding: 0px;">
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"target="new">Archivo Histórico de la Policía Nacional</a> (Guatemala City, Guatemala)</li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"target="new">Estadio Nacional</a> (Santiago, Chile)</li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a
+style="color: rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"target="new">Museo de la Memoria y los Derechos Humanos</a> (Santiago, Chile)</li>
+</ul>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">Join Today!</div>
+<p style="padding-left:8px; margin:10px 0px 10px 0px;">Join the worldwide movement of organizations and individuals dedicated to helping historic sites inspire social consciousness and action. <a style="color:#3397ba;" href="http://www.sitesofconscience.org/join-us/en/">Sign up today!</a></p>
+<div style="color:#fff; background-color:#8dc13f;font-size:12px;font-weight:bold;padding:3px 8px 3px 8px;">About Us</div>
+<p style="padding-left:8px; margin:10px 0px 10px 0px;">The International Coalition of Sites of Conscience is a worldwide network of historic sites dedicated to remembering past struggles for justice and addressing their contemporary legacies. <a style="color:#3397ba;" href="http://www.sitesofconscience.org/about-us/en/">More about us</a>.</p>
+</div>
+</td>
+<td bgcolor="#8dc13f"></td>
+<td colspan="3" valign="top" style="padding:13px; font-size:12px; line-height:18px;">
+<div style="margin:0px 13px 0px 13px;">
+<h3 style="font-weight:normal;font-size:14px;color:#9a9a9a;margin:0px;">May 2010</h3>
+<br>
+Alongside the traditional tourism of ancient temples and beautiful scenery in Cambodia, places associated with the Khmer Rouge regime are increasingly making it on to travelers’ checklists. Cambodia recently announced plans to <a style="color: rgb(51, 151, 186);"
+href="http://www.usatoday.com/travel/destinations/2010-03-11-cambodia-khmer-rouge-sites_N.htm"target="new"> preserve and develop 14 sites in Anlong Veng</a>, the last stronghold of the Khmer Rouge, which claimed millions of victims between 1975 and 1979. The government believes these sites “will increase understanding of the Khmer Rouge’s last political leadership,” but critics cast it as a <a style="color: rgb(51, 151, 186);"
+href="http://www.independent.co.uk/news/world/asia/cambodia-puts-the-cremation-site-of-pol-pot-on-historic-tourist-trail-1919472.html"target="new">
+“scheme to profit from the country’s genocidal past.”</a> Around the world, people flock to places remembering painful histories; some describe this as “dark tourism” and criticize such sites for trivializing or exploiting suffering. But Sites of Conscience that remember atrocities resist being simplified as sites of suffering whose visitors are passive voyeurs. Instead, Sites of Conscience intentionally invite all visitors - locals, tourists, students, and the general public - to be active participants in addressing the issues the site raises, past and present.<div style="float: left; margin: 4px 10px 1px 0px; width: 275px;
+color: rgb(153, 153, 153); font-size: 11px; line-height: 14px;"><img
+src="http://www.sitesofconscience.org/wp-content/uploads/Tuol-Sleng.jpg" style="margin-bottom: 3px;"
+border="0" height="195" width="275"><br>Students discuss the history and legacies of the Khmer Rouge at the Tuol Sleng Genocide Museum.</div> <br> In Cambodia, the <a style="color: rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/activities/new-programs/en/#Tuol Sleng" target="new">Tuol Sleng Genocide Museum</a>, a former detention center, is engaging local youth in a program that interrogates the site’s history to catalyze action. Students explore the site, discuss its history and legacies today and decide on actions they would like to take to shape Cambodian society. 
+In Chile, survivors from the <a
+style="color: rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/sites/villa-grimaldi/en/"
+target="new">Villa Grimaldi Peace Park</a>, a former secret torture and detention center from the Pinochet dictatorship, lead interactive tours that connect the site’s history to social concerns today and engage visitors in conversation about their own role in human rights. This kind of “Conscience tourism” strives to re-imagine both the role of Sites and their visitors.
+<p></p>
+<p> Sites of Conscience continue to explore new ways of using the past to fuel action today:</p>
+<ul style="list-style-type: none; margin: 0px; padding: 0px;">
+<li style="background-image: 
+url(&quot;http://members.sitesofconscience.org/a/soc-newsletter/bullet.png&quot;);
+ background-repeat: no-repeat; padding: 0px 0px 10px 30px; min-height: 
+50px;">  <div style="float: right; margin: 4px 0px 1px 10px; width: 150px;
+color: rgb(153, 153, 153); font-size: 11px; line-height: 14px;"><img
+src="http://www.sitesofconscience.org/wp-content/uploads/Liberia.jpg" style="margin-bottom: 3px;" border="0"
+height="212" width="150"><br> Liberians create body maps to illustrate
+their histories.</div> Last month, Liberians took a crucial step
+towards implementing a key recommendation of their Truth and
+Reconciliation Commission: memorialization of the country’s civil wars.
+Through an exchange with South Africa’s Human Rights Media Center, Liberia’s Civic Initiative and Liberia Media Center launched the country’s first art
+and memory workshop where participants could <a style="color: rgb(51,
+151, 186);"
+href="http://www.sitesofconscience.org/activities/en/#Liberia"
+target="new">“WITNESS - REMEMBER - CREATE”</a> personal and public memories of the Liberian conflict.</li>
+<br>
+<li style="background-image: 
+url(&quot;http://members.sitesofconscience.org/a/soc-newsletter/bullet.png&quot;);
+ background-repeat: no-repeat; padding: 0px 0px 10px 30px; min-height: 
+50px;"> 
+After nearly a decade of struggle, the Museum of Memory of Rosario <a style="color: rgb(51,
+151, 186);"
+href="http://www.sitesofconscience.org/activities/en/#Rosario"
+target="new">received the keys</a> to a
+former army headquarters used during Argentina’s last dictatorship
+(1976-1983). Supported by the International Coalition’s <a style="color:
+rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/resources/project-support-fund/en/"
+target="new">Project Support Fund</a>, the museum is now designing an innovative program that will harness the power of this
+historic site to inspire Rosario’s youth to <a
+style="color: rgb(51, 151, 186);"
+href="http://www.sitesofconscience.org/activities/en/#RosarioProgram"
+target="new">“open our eyes”</a> towards
+injustice and indifference today.</li>
+<br>
+<li style="background-image: 
+url(&quot;http://members.sitesofconscience.org/a/soc-newsletter/bullet.png&quot;);
+ background-repeat: no-repeat; padding: 0px 0px 10px 30px; min-height: 
+50px;"> <div style="float: left; margin: 4px 10px 1px 0px; width: 200px;
+color: rgb(153, 153, 153); font-size: 11px; line-height: 14px;"><img
+src="http://www.sitesofconscience.org/wp-content/uploads/Workers-Museum.jpg" style="margin-bottom: 3px;"
+border="0" height="141" width="200"><br>Workers Museum reopens as a
+“Site of Labor Migration.”</div>Amid rising hostility towards new
+migrants, former migrant workers, now part of the fabric of Newtown,
+South Africa, shared their experiences at the <a style="color: rgb(51,
+151, 186);"
+href="http://www.sitesofconscience.org/activities/en/#WorkersMuseum"
+target="new"> reopening of the Workers Museum</a> as a “Site of Labor
+Migration”. The former compound and hostel for migrant workers invites
+visitors to explore the site’s rich history and personal stories of
+former residents and reflect on migrant life - then and now.</li>
+<p>To unsubscribe click <a style="color:
+rgb(51, 151, 186);"href="mailto:coalition@sitesofconscience.org">here</a>.
+</ul>
+</div>
+</td>
+</tr>
+</table>
+</div>
+</body></html>
