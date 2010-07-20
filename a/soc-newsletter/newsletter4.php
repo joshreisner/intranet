9917840170f07c9290e02aa8b927e91f8f3a281a
joshreisner <josh@joshreisner.com>
2010-07-20 21:19:23 +0000

    adding newsletter for eva

---
+++ a/soc-newsletter/newsletter4.php
@@ -0,0 +1,196 @@
+﻿<html><head>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px}
+-->
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Newsletter</title>
+</head>
+<body style="background-color:#fff;margin:0px;">
+<style type="text/css">
+<!--
+body{background-color:#fff; margin:0px} 
+-->
+</style>
+<div style="padding:15px;">
+<table width="650" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family:Arial,Helvetica,sans-serif; font-size:11px; line-height:16px;">
+<tr>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header1.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header2.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="85" height="117"><img src="http://members.sitesofconscience.org/a/soc-newsletter/header3.jpg" alt="header1" width="85" height="117" border="0"/></td>
+<td width="5" bgcolor="#8dc13f"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td>
+<td width="380" bgcolor="#3397ba" align="center"><img border=0 src="http://members.sitesofconscience.org/a/soc-newsletter/newsletter-logo.png" alt="logo" width="380" height="117"/></td>
+</tr>
+<tr><td colspan="7" bgcolor="#8dc13f" height="5"><img src="http://members.sitesofconscience.org/a/soc-newsletter/spacer.png" width="5" height="5" border="0"/></td></tr>
+<tr>
+<td colspan="3" valign="top" style="padding:8px;">
+<div style="margin: 8px 0px;">
+<div style="color: rgb(255, 255, 255); background-color: rgb(141, 193, 
+63); font-size: 12px; font-weight: bold; padding: 3px 8px;">Memory 
+Issues in the News</div>
+<ul style="list-style-type: none; margin: 10px 0px; padding: 0px;">
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.guardian.co.uk/world/2010/jun/07/bhopal-disaster-india-sentences"
+ target="new">Bhopal campaigners condemn 'insulting' sentences over disaster</a></li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://news.bbc.co.uk/2/hi/northern_ireland/10320609.stm" target="new">
+Bloody Sunday report published</a></li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://news.bbc.co.uk/2/hi/world/us_and_canada/10338836.stm" target="new">
+Native Canadians to recall boarding school abuse</a></li>
+</ul>
+<div style="color: rgb(255, 255, 255); background-color: rgb(141, 193, 
+63); font-size: 12px; font-weight: bold; padding: 3px 8px;">Announcements </div>
+<ul style="list-style-type: none; margin: 10px 0px; padding: 0px;">
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/activities/en/#AAM"
+ target="new">ICOM–U.S.Awards 2010 International Service Citation to Sites of Conscience</a></li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/board/en/#West"
+ target="new">W. Richard West Jr. Announces Candidacy for ICOM President</a></li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/board/en/#Timerman"
+ target="new">
+Héctor Timerman Appointed as Argentina’s New Foreign Minister </a></li>
+</ul>
+<div style="color: rgb(255, 255, 255); background-color: rgb(141, 193, 
+63); font-size: 12px; font-weight: bold; padding: 3px 8px;">New 
+Institutional Members</div>
+<ul style="list-style-type: none; margin: 10px 0px; padding: 0px;">
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"
+ target="new">Manzanar National Historic Site</a> (Los Angeles, U.S.A.)</li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"
+ target="new">Museum of Tolerance</a> (Independence, U.S.A.)</li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"
+ target="new">Sabarmati Ashram Preservation and Memorial Trust</a> (New Delhi, India)</li>
+<li style="margin: 0px 0px 5px; padding: 0px 0px 0px 8px;"><a 
+style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/institutional-members/en/"
+ target="new">Skirball Cultural Center</a> (Los Angeles, U.S.A.)</li>
+</ul>
+<div style="color: rgb(255, 255, 255); background-color: rgb(141, 193, 
+63); font-size: 12px; font-weight: bold; padding: 3px 8px;">Join Today!</div>
+<p style="padding-left: 8px; margin: 10px 0px;">Join the worldwide 
+movement of organizations and individuals dedicated to helping historic 
+sites inspire social consciousness and action. <a style="color: rgb(51, 
+151, 186);" href="http://www.sitesofconscience.org/join-us/en/">Sign up 
+today!</a></p>
+<div style="color: rgb(255, 255, 255); background-color: rgb(141, 193, 
+63); font-size: 12px; font-weight: bold; padding: 3px 8px;">About Us</div>
+<p style="padding-left: 8px; margin: 10px 0px;">The International 
+Coalition of Sites of Conscience is a worldwide network of historic 
+sites dedicated to remembering past struggles for justice and addressing
+ their contemporary legacies. <a style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/about-us/en/">More about us</a>.</p>
+</div>
+</td>
+<td bgcolor="#8dc13f"></td>
+<td colspan="3" style="padding: 13px; font-size: 12px; line-height: 
+18px;" valign="top">
+<div style="margin: 0px 13px;">
+<h3 style="font-weight: normal; font-size: 14px; color: rgb(154, 154, 
+154); margin: 0px;">July 2010</h3>
+<br>
+<a style="color: rgb(51, 151, 186);" 
+href="http://www.nytimes.com/2010/04/18/opinion/18sun3.html?_r=1"target="new"> “Over the edge”</a> or <a style="color: rgb(51, 151, 186);" 
+href="http://www.philly.com/inquirer/world_us/20100429_For_many_in_Arizona__a_law_long_overdue.html" target="new">“overdue”</a>? Arizona’s new <a style="color: rgb(51, 151, 186);"href=" http://edition.cnn.com/2010/POLITICS/04/23/immigration.faq/index.html" target="new"> immigration law</a>, which allows local police to arrest anyone unable to verify their immigration status, has divided Americans. Fearing this law will open the door to rampant discrimination and racial profiling, critics have expressed their outrage with boycotts and protest. But supporters have embraced the law as a positive step towards national security and economic growth. These clashes reflect a long-rooted anxiety about immigration issues in the United States. Meanwhile in Europe,  <a style="color: rgb(51, 
+151, 186);" 
+href=" http://www.nytimes.com/2009/06/23/world/europe/23france.html?_r=1&scp=1&sq=france%20burqa&st=cse"
+ target="new"> France </a>, <a style="color: rgb(51, 151, 186);" 
+href=" http://www.cnn.com/2010/WORLD/europe/04/29/belgium.burqa.ban/index.html" 
+target="new"> Belgium </a> and <a style="color: rgb(51, 151, 186);" 
+href=" http://www.nytimes.com/2010/06/24/world/europe/24iht-spain.html"target="new"> Spain</a> have made headlines with their own immigration-related controversy. Taking steps to ban the wearing of burqas in public, the countries have fueled divisive debates on religious freedom, discrimination and xenophobia. The myriad opinions and concerns rising across America and Europe call for spaces for dialogue where all voices can be heard.
+<a style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/resources/networks/immigration/en/"target="new"> Immigration Sites of Conscience</a> offer unique opportunities for constructive dialogue by enabling visitors to learn from immigration history to understand today’s debates, and by inviting them to share their own experiences and perspectives. <div style="float: left; margin: 4px 10px 1px 
+0px; width: 275px; color: rgb(153, 153, 153); font-size: 11px; 
+line-height: 14px;"><img 
+src="http://www.sitesofconscience.org/wp-content/uploads/Bois-du-Cazier.jpg" 
+style="margin-bottom: 3px;" border="0" height="195" width="275"><br>Le Bois du Cazier, one of the sites to open transatlantic dialogue on immigration and xenophobia.</div> Now, through <a style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/activities/en/#MCCA"target="new"><i>Navigating Difference</i></a>, a new interactive installation and program, Sites of Conscience are taking this conversation transnational.  The <a style="color: rgb(51, 151, 186);" 
+href="http://www.nps.gov/elis/index.htm"target="new"> Ellis Island Immigration Museum</a> in the U.S., the Italy's <a style="color: rgb(51, 151, 186);" 
+href="http://www.museidigenova.it/spip.php?rubrique24"target="new">Mu.MA/Galata Museo del Mare</a> and Belgium’s <a style="color: rgb(51, 151, 186);" 
+href=" http://www.leboisducazier.be/"target="new"> Le Bois du Cazier</a>, each tracing different paths that migrants have journeyed through time, will simultaneously invite visitors to answer a common question such as “Does immigration benefit my community?”. Through the installation, visitors will share their opinions and see the responses from within their communities and from the sites in other countries. Beginning with a simple question, this new program will engage visitors in conversations that can be the first step in ‘navigating differences’ on immigration in Europe and the United States.
+
+<p></p>
+<p>Around the world, Sites of Conscience are opening new dialogues on today’s critical issues:</p>
+<ul style="list-style-type: none; margin: 0px; padding: 0px;">
+<li style="background-image: 
+url(&quot;http://members.sitesofconscience.org/a/soc-newsletter/bullet.png&quot;);
+ background-repeat: no-repeat; padding: 0px 0px 10px 30px; min-height: 
+50px;">  <div style="float: right; margin: 4px 0px 1px 10px; width: 
+200px; color: rgb(153, 153, 153); font-size: 11px; line-height: 14px;"><img
+ src="http://www.sitesofconscience.org/wp-content/uploads/District-Six.jpg" style="margin-bottom: 
+3px;" border="0" height="120" width="200"><br>District Six Museum's exhibit on football explores a history of racism in the game.</div> With World Cup fever still in the air, <a style="color: rgb(51, 151, 186);"href="http://www.sitesofconscience.org/sites/district-six/en/"target="new">District Six Museum’s</a> (South Africa) new exhibit examines what happens off the field as it explores the history of football in South Africa.  <a style="color: rgb(51, 151, 186);" 
+href="http://www.sitesofconscience.org/activities/en/#offside"target="new"><i>Offside: Kick Ignorance Out! Football Unites, Racism Divides</i></a> explores the prejudice many renowned South African football players encountered and encourages visitors to question how racism impacts one of the world’s favorite sports.
+</li>
+<br>
+<li style="background-image: 
+url(&quot;http://members.sitesofconscience.org/a/soc-newsletter/bullet.png&quot;);
+ background-repeat: no-repeat; padding: 0px 0px 10px 30px; min-height: 
+50px;"> 
+<a style="color: rgb(51, 151, 186);"href="http://www.slcgg.org/"target="new">Campaign for Good Governance (CGG)</a> (Sierra Leone) is launching <a style="color: rgb(51, 151, 186);"href="http://www.sitesofconscience.org/activities/en/#CGG"target="new">new public dialogues</a> at Sierra Leone’s Special Court where the crimes of the country’s civil war (1991-2002) were prosecuted. Stakeholders, community members and students will explore issues of justice and accountability and how the iconic site could become a space for healing and reconciliation. At the Coalition’s <a style="color: rgb(51, 151, 186);"href="http://www.sitesofconscience.org/activities/en/#workshop"target="new">first West African workshop</a> in Sierra Leone, CGG shared this initiative with sites from Liberia, Senegal, Kenya and Uganda. Together they identified strategies to develop places of memory as centers for dialogue on unresolved questions around justice and impunity in post-conflict democracies.
+</li>
+<br>
+<li style="background-image: 
+url(&quot;http://members.sitesofconscience.org/a/soc-newsletter/bullet.png&quot;);
+ background-repeat: no-repeat; padding: 0px 0px 10px 30px; min-height: 
+50px;"> <div style="float: left; margin: 4px 10px 1px 0px; width: 200px;
+ color: rgb(153, 153, 153); font-size: 11px; line-height: 14px;"><img 
+src="http://www.sitesofconscience.org/wp-content/uploads/Building-a-City-and-a-Nation.jpg" 
+style="margin-bottom: 3px;" border="0" height="141" width="200"><br>Portraits tell the stories of numerous immigrants that crossed the U.S.-Mexican border in the 1930s and 1940s. Photo: Casasola Studio photographs, PH041. University of Texas at El Paso Library, Special Collections Department</div> In El Paso, Texas, the largest border community in the U.S., the <a style="color: rgb(51, 151, 186);"href="http://academics.utep.edu/Default.aspx?tabid=20021"target="new">University of Texas El Paso (UTEP)</a> is gathering numerous portraits, images and personal stories in a <a style="color: rgb(51, 151, 186);"href="http://www.sitesofconscience.org/activities/en/#UTEP"target="new">new exhibition</a> that tells the tale of the thousands who crossed the border from Mexico during the 1930s and 1940s. Highlighting the different experiences of these immigrants and connecting them to immigration today, UTEP is providing new opportunities for reflection and engaged conversation on the complexities of immigration past and present. 
+</li>
+<p> Become part of our growing worldwide network of organizations and individuals dedicated to helping historic sites inspire social consciousness and action. <a style="color: rgb(51, 
+151, 186);" href="http://www.sitesofconscience.org/join-us/en/">Sign up today!</a><br>To subscribe or unsubscribe from this newsletter click <a style="color: rgb(51, 151, 186);" 
+href="mailto:coalition@sitesofconscience.org">here</a>.
+</p></ul>
+</div>
+</td>
+</tr>
+</tbody></table>
+</div>
+</body></html>
