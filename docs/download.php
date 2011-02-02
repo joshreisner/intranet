b6e5f80d4dda544f8499f95af96e382b4be3d115
joshreisner <josh@joshreisner.com>
2011-02-02 19:41:36 +0000

    refactoring doc download to prevent errors

--- docs/download.php
+++ docs/download.php
@@ -1,15 +1,17 @@
-<?
-include("../include.php");
+<?php
+include('../include.php');
 
-$d = db_grab("SELECT 
+url_query_require();
+
+$d = db_grab('SELECT 
 		d.title, 
 		t.extension, 
 		d.content 
 	FROM docs d 
 	JOIN docs_types t ON d.type_id = t.id
-	WHERE d.id = " . $_GET["id"]);
+	WHERE d.id = ' . $_GET['id']);
 
-db_query("INSERT INTO docs_views ( documentID, userID, viewedOn ) VALUES ( {$_GET["id"]}, {$_SESSION["user_id"]}, GETDATE() )");
+db_query('INSERT INTO docs_views ( documentID, userID, viewedOn ) VALUES ( ' . $_GET['id'] . ', ' . user() . ', ' . db_date() . ' )');
 
-file_download($d["content"], $d["title"], $d["extension"])
+file_download($d['content'], $d['title'], $d['extension'])
 ?>
