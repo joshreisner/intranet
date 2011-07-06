55227ec8902e6426d1d0da68b3865c77c6b43ac6
joshreisner <josh@joshreisner.com>
2011-07-06 21:52:11 +0000

    fixing 'other' option for locations

--- staff/locations.php
+++ staff/locations.php
@@ -3,9 +3,9 @@ include('include.php');
 
 echo drawTop();
 
-if (!isset($_GET['id'])) {
-	$_GET['id'] = 1;
-	$_josh['request']['path_query'] = '/staff/locations.php?id=1';
+if (!isset($_GET['office_id'])) {
+	$_GET['office_id'] = 1;
+	$_josh['request']['path_query'] = '/staff/locations.php?office_id=1';
 }
 
 $locations = db_query('SELECT o.id, o.name' . langExt() . ' FROM offices o WHERE o.is_active = 1 AND (SELECT COUNT(*) FROM users u WHERE u.is_active = 1 AND u.officeID = o.id) > 0 ORDER BY o.precedence');
@@ -14,23 +14,23 @@ if (db_found($locations)) {
 	$counter = 1;
 	while ($l = db_fetch($locations)) {
 		if ($counter < 6) {
-			$pages['/staff/locations.php?id=' . $l['id']] = $l['name'];
+			$pages['/staff/locations.php?office_id=' . $l['id']] = $l['name'];
 		} else{
 			if ($counter == 6) {
-				$pages['/staff/locations.php?id=other'] = 'Other';
+				$pages['/staff/locations.php?office_id=other'] = 'Other';
 				$others = array();
 			}
-			$others[] = 'u.officeID <> ' . $l['id'];
+			$others[] = $l['id'];
 		}
 		$counter++;
 	}
 	echo drawNavigation($pages, 'path_query');
 }
 
-if ($_GET['id'] == 'other') {
-	echo drawStaffList('u.is_active = 1 AND ' . implode($others, ' AND '));
+if ($_GET['office_id'] == 'other') {
+	echo drawStaffList('u.is_active = 1 AND u.officeID IN (' . implode(',', $others) . ')');
 } else {
-	echo drawStaffList('u.is_active = 1 and u.officeID = ' . $_GET['id']);
+	echo drawStaffList('u.is_active = 1 and u.officeID = ' . $_GET['office_id']);
 }
 
 echo drawBottom();
