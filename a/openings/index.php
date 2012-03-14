a4d1b4c236e6385d1ef71c2fed70e001b74138fe
joshreisner <josh@joshreisner.com>
2012-03-14 11:17:09 +0000

    making tweaks to job openings per lauren

--- a/openings/index.php
+++ a/openings/index.php
@@ -1,68 +1,74 @@
-<?
-include("../../include.php");
-
-if (url_action("delete")) {
-	db_delete("openings");
-	url_drop();
-} elseif ($posting) {
-	//debug();
-	if ($id = db_save("openings")) url_change("position.php?id=" . $id);
-}
-
+<?php
+include('../../include.php');
 echo drawTop();
-?>
-<table class="left" cellspacing="1">
-	<? if ($page['is_admin']) {
-		$colspan = 4;
-		echo drawHeaderRow("Open Positions", $colspan, "new", "#bottom");
-	} else {
-		$colspan = 3;
-		echo drawHeaderRow("Open Positions", $colspan);
-	}?>
-	<tr>
-		<th width="50%">Title</th>
-		<th width="30%">Location</th>
-		<th class="r" width="20%"><nobr>Last Update</nobr></th>
-		<? if ($page['is_admin']) {?><th></th><? }?>
-	</tr>
-	<?
-	$result = db_query("SELECT 
-							j.id,
-							j.title,
-							c.description corporationName,
-							o.name office,
-							ISNULL(j.updated_date, j.created_date) updated_date
-						FROM openings j
-						LEFT JOIN organizations c ON j.corporationID = c.id
-						LEFT JOIN offices o ON j.officeID = o.id
-						WHERE j.is_active = 1
-						ORDER BY c.description, j.title");
-	$lastCorporation = "";
-	while ($r = db_fetch($result)) {
-	if ($r["corporationName"] != $lastCorporation) {
-		$lastCorporation = $r["corporationName"];
-		echo '<tr class="group"><td colspan="' . $colspan . '">' . $lastCorporation . '</td></tr>';
-		}?>
+db_switch('seedco_wp');
+
+if (url_id()) {
+	//job page
+	$r = db_grab('SELECT 
+			p.id,
+			p.post_title "title",
+			p.post_status "status",
+			p.post_content "description",
+			p.post_modified "updated_date",
+			(SELECT m.meta_value FROM wp_postmeta m WHERE m.post_id = p.id AND m.meta_key = "job-location") "location"
+		FROM wp_posts p
+		WHERE p.id = ' . $_GET['id']);
+	?>
+	<table class="left" cellspacing="1">
+		<?=drawHeaderRow("View Position", 2)?>
 		<tr>
-			<td><a href="position.php?id=<?=$r["id"]?>"><?=$r["title"]?></a></td>
-			<td><?=$r["office"]?></td>
-			<td class="r"><?=format_date($r["updated_date"])?></td>
-			<?=drawdrawColumnDelete("Delete this position?", $r["id"])?>
+			<td class="left">Location</td>
+			<td><?=$r['location']?></td>
 		</tr>
-		<? }?>
-</table>
-
-<a name="bottom"></a>
-
-<? if ($page['is_admin']) {
-	$form = new intranet_form;
-	$form->addRow("itext",  "Title" , "title", "", "", true);
-	$form->addRow("select", "Organization" , "corporationID", "SELECT id, title from organizations ORDER BY title", "", true);
-	$form->addRow("select", "Location" , "officeID", "SELECT id, name FROM offices ORDER BY precedence", "", true);
-	$form->addRow("checkbox", "Internship?" , "is_internship", $r["is_internship"]);
-	$form->addRow("textarea", "Description" , "description", "", "", true);
-	$form->addRow("submit"  , "post open position");
-	$form->draw("Add an Open Position");
+		<tr>
+			<td class="left">Position</td>
+			<td class="text">
+				<h1><?=$r['title']?></h1><br/>
+				<?=$r['description']?>
+			</td>
+		</tr>
+		<tr>
+			<td class="left">Updated</td>
+			<td><?=format_date($r['updated_date'])?></td>
+		</tr>
+	</table>
+	<?php
+} else {
+	//jobs list
+?>
+	<table class="left" cellspacing="1">
+		<?=drawHeaderRow('Open Positions', 2)?>
+		<tr>
+			<th width="80%">Title</th>
+			<th class="r" width="20%"><nobr>Last Update</nobr></th>
+		</tr>
+		<?php
+		$result = db_query('SELECT 
+			p.id,
+			p.post_title "title",
+			p.post_status "status",
+			p.post_content "content",
+			p.post_modified "updated_date",
+			(SELECT m.meta_value FROM wp_postmeta m WHERE m.post_id = p.id AND m.meta_key = "job-location") "location"
+		FROM wp_posts p
+		WHERE post_type = "careers" AND (post_status = "publish" or post_status = "draft")
+		ORDER BY 6');
+		
+		$lastLocation = "";
+		while ($r = db_fetch($result)) {
+		if ($r['location'] != $lastLocation) {
+			$lastLocation = $r['location'];
+			echo '<tr class="group"><td colspan="2">' . $lastLocation . '</td></tr>';
+			}?>
+			<tr>
+				<td><a href="./?id=<?=$r['id']?>"><?=$r['title']?></a></td>
+				<td class="r"><?=format_date($r['updated_date'])?></td>
+			</tr>
+			<? }?>
+	</table>
+<?php
 }
-
-echo drawBottom(); ?>
+db_switch();
+echo drawBottom();
+?>
