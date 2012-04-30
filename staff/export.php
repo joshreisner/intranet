bec569aab82ff0240b83bf54abcb195f70c68353
joshreisner <josh@joshreisner.com>
2012-04-30 22:49:44 +0000

    fixing export for jeff

--- staff/export.php
+++ staff/export.php
@@ -1,23 +1,21 @@
 <?php
-include("../include.php");
+include('../include.php');
 
-$result = db_table("SELECT
+$result = db_table('SELECT
 	u.id link,
 	u.firstname,
 	u.lastname,
 	u.title,
 	d.departmentName department,
-	o.description organization,
+	o.title organization,
 	u.email,
-	(SELECT CASE WHEN u.rankID = 9 THEN 'No' ELSE 'Yes' END) is_staff
+	(SELECT CASE WHEN u.rankID = 9 THEN "No" ELSE "Yes" END) is_staff
 	FROM users u
-	JOIN departments d ON u.departmentID = d.departmentID
+	LEFT JOIN departments d ON u.departmentID = d.departmentID
 	JOIN organizations o ON u.organization_id = o.id
 	WHERE u.is_active = 1
-	ORDER BY u.lastname, u.firstname");
-$base = url_base();
-foreach ($result as &$r) {
-	$r["link"] = draw_link($base . "/staff/view.php?id=" . $r["link"], $r["link"]);
-}
-file_array($result, "employees");
-?>
+	ORDER BY u.lastname, u.firstname');
+
+foreach ($result as &$r) $r['link'] = draw_link(url_base() . '/staff/view.php?id=' . $r['link'], $r['link']);
+
+file_array($result, 'employees');
